115e9672bf3d0725a1f3c8f0b8572b29957d1f98
Dominik Schloesser <dsc@dosc.net>
2018-05-16 17:03:27 +0200

    start web server

---
+++ web.h
@@ -0,0 +1,89 @@
+// web.h
+#pragma once
+
+#if defined(__ESP__)
+
+#include <functional>
+
+// ESP32: patch required currently: #if defined(ESP8266) || defined(ESP32)
+#include <PubSubClient.h>
+
+#include "platform.h"
+#include "array.h"
+#include "map.h"
+
+#include "scheduler.h"
+
+#include <ArduinoJson.h>
+
+namespace ustd {
+class Web {
+  public:
+    Scheduler *pSched;
+    bool isOn = false;
+    bool netUp = false;
+    String webServer;
+
+    Web() {
+        webServer = "";
+        isOn = false;
+    }
+
+    ~Web() {
+        if (isOn) {
+            isOn = false;
+        }
+    }
+
+    void begin(Scheduler *_pSched) {
+        // Make sure _clientName is Unique! Otherwise WEB server will rapidly
+        // disconnect.
+        pSched = _pSched;
+
+        // give a c++11 lambda as callback scheduler task registration of
+        // this.loop():
+        std::function<void()> ft = [=]() { this->loop(); };
+        pSched->add(ft);
+
+        std::function<void(String, String, String)> fnall =
+            [=](String topic, String msg, String originator) {
+                this->subsMsg(topic, msg, originator);
+            };
+        pSched->subscribe("#", fnall);
+
+        pSched->publish("net/network/get");
+        pSched->publish("net/services/webserver/get");
+        isOn = true;
+    }
+
+    void loop() {
+        if (isOn) {
+        }
+    }
+
+    void subsMsg(String topic, String msg, String originator) {
+        DynamicJsonBuffer jsonBuffer(200);
+        JsonObject &root = jsonBuffer.parseObject(msg);
+        if (!root.success()) {
+            // DBG("web: Invalid JSON received: " + String(msg));
+            return;
+        }
+        if (topic == "net/services/webserver") {
+            webServer = root["server"].as<char *>();
+        }
+        if (topic == "net/network") {
+            String state = root["state"];
+            if (state == "connected") {
+                if (!netUp) {
+                    netUp = true;
+                }
+            } else {
+                netUp = false;
+            }
+        }
+    };
+};  // namespace ustd
+
+}  // namespace ustd
+
+#endif  // defined(__ESP__)
