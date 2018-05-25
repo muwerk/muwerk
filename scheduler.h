bf596405e294d530300a923bb0093bdebe876209
Dominik Schloesser <dsc@dosc.net>
2018-05-25 10:47:13 +0200

    valgrind checks: clean-up taskEntry names in destructor

--- scheduler.h
+++ scheduler.h
@@ -120,6 +120,10 @@ class Scheduler {
 
 #ifndef __ATTINY__
     virtual ~Scheduler() {
+        for (int i = 0; i < taskList.length(); i++) {
+            if (taskList[i].szName != nullptr)
+                free(taskList[i].szName);
+        }
         int l = msgqueue.length();
         for (int i = 0; i < l; i++) {
             msgqueue.pop();
