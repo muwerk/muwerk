1bb7dd950b5a0ce96dcc7be56a91d27c1194eff4
Dominik Schlösser <dsc@dosc.net>
2019-08-25 17:07:30 +0200

    Some inline comments

--- Examples/minimal/mu_minimal.cpp
+++ Examples/minimal/mu_minimal.cpp
@@ -10,20 +10,23 @@ int led;
 void appLoop();
 
 void task0(String topic, String msg, String originator) {
-
+    // when receiving a message published to subscribed topic "led", do:
     if (msg == "on")
         digitalWrite(LED_BUILTIN, LOW);  // Turn the LED on
     if (msg == "off")
         digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off
 }
 
-void task1() {
+void task1() { // scheduled every 50ms
     static int s1 = 0;
     static unsigned long t1;
     if (ustd::timeDiff(t1, millis()) > 500L) {
         if (s1 == 0) {
             s1 = 1;
-            sched.publish("led", "on");
+            sched.publish("led", "on"); // MQTT-style publish to topic "led"
+            // If munet with mqtt is used, those messages are transparently send 
+            // to an external MQTT-server, by default, muwerk only dispatches
+            // them between local tasks.
         } else {
             s1 = 0;
             sched.publish("led", "off");
@@ -38,9 +41,9 @@ void setup() {
     pinMode(LED_BUILTIN, OUTPUT);
 
     int tID = sched.add(appLoop, "main");
-    sched.subscribe(tID, "led", task0);
+    sched.subscribe(tID, "led", task0); // subscribe to MQTT-like top "led"
 
-    sched.add(task1, "task1", 50000L);
+    sched.add(task1, "task1", 50000L); // execute task1 every 50ms (50000us)
 }
 
 void appLoop() {
