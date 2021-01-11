af49fa02792a095c6998cada36f2385db46c5f34
Dominik Schlösser <dsc@dosc.net>
2021-01-11 17:12:50 +0100

    Doc image url update

--- docs/search/all_a.js
+++ docs/search/all_a.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['scheduler_50',['Scheduler',['../classustd_1_1Scheduler.html#a08fb718f763d4db0bc3f4fd58a8dfbde',1,'ustd::Scheduler::Scheduler()'],['../classustd_1_1Scheduler.html',1,'ustd::Scheduler']]],
-  ['sensorprocessor_51',['sensorprocessor',['../classustd_1_1sensorprocessor.html#af3295232cb60ec9e9a331700875d4961',1,'ustd::sensorprocessor::sensorprocessor()'],['../classustd_1_1sensorprocessor.html',1,'ustd::sensorprocessor']]],
+  ['scheduler_50',['Scheduler',['../classustd_1_1Scheduler.html',1,'ustd::Scheduler'],['../classustd_1_1Scheduler.html#a08fb718f763d4db0bc3f4fd58a8dfbde',1,'ustd::Scheduler::Scheduler()']]],
+  ['sensorprocessor_51',['sensorprocessor',['../classustd_1_1sensorprocessor.html',1,'ustd::sensorprocessor'],['../classustd_1_1sensorprocessor.html#af3295232cb60ec9e9a331700875d4961',1,'ustd::sensorprocessor::sensorprocessor()']]],
   ['shift_52',['shift',['../classustd_1_1Console.html#ab4909c6677b87eba5a8e65cc60826d75',1,'ustd::Console']]],
   ['singletaskmode_53',['singleTaskMode',['../classustd_1_1Scheduler.html#a1916e661e513a686535e79367c16f5bd',1,'ustd::Scheduler']]],
   ['split_54',['split',['../classustd_1_1JsonFile.html#a24de21d2903e260c2b78168683a5c714',1,'ustd::JsonFile']]],
