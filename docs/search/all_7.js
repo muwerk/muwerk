cc20405c7844dd785f9a84eb44808ddc1892507d
Moll, Leo <leo.moll@robot5.de>
2021-01-11 17:00:15 +0100

    Doc for 0.4.0 release

--- docs/search/all_7.js
+++ docs/search/all_7.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['muwerk_20a_20cooperative_20scheduler_20wit_20mqtt_2dlike_20communication_20queues_29',['muwerk a cooperative scheduler wit MQTT-like communication queues',['../index.html',1,'']]],
+  ['mqttmatch_29',['mqttmatch',['../classustd_1_1Scheduler.html#a38d3c7d07aa6cf4ec48baa1f4d74698b',1,'ustd::Scheduler']]],
   ['muwerk_30',['muwerk',['../md_README.html',1,'']]],
-  ['mqttmatch_31',['mqttmatch',['../classustd_1_1Scheduler.html#a38d3c7d07aa6cf4ec48baa1f4d74698b',1,'ustd::Scheduler']]]
+  ['muwerk_20a_20cooperative_20scheduler_20wit_20mqtt_2dlike_20communication_20queues_31',['muwerk a cooperative scheduler wit MQTT-like communication queues',['../index.html',1,'']]]
 ];
