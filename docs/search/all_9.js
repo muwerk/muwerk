a4b8637136d0bce88158a108bf95ea3ee9bbb222
Leo Moll <leo.moll@yeasoft.com>
2021-01-15 02:02:27 +0100

    Rebuilt documentation

--- docs/search/all_9.js
+++ docs/search/all_9.js
@@ -3,5 +3,5 @@ var searchData=
   ['metronome_40',['metronome',['../classustd_1_1metronome.html#a8966b8be32f4e6808c6300b69a2136bf',1,'ustd::metronome::metronome()'],['../classustd_1_1metronome.html',1,'ustd::metronome']]],
   ['mqttmatch_41',['mqttmatch',['../classustd_1_1Scheduler.html#a38d3c7d07aa6cf4ec48baa1f4d74698b',1,'ustd::Scheduler']]],
   ['muwerk_42',['muwerk',['../md_README.html',1,'']]],
-  ['muwerk_20a_20cooperative_20scheduler_20with_20mqtt_2dlike_20communication_20queues_43',['muwerk a cooperative scheduler with MQTT-like communication queues',['../index.html',1,'']]]
+  ['muwerk_20is_20a_20cooperative_20scheduler_20with_20mqtt_2dlike_20communication_20queues_43',['muwerk is a cooperative scheduler with MQTT-like communication queues',['../index.html',1,'']]]
 ];
