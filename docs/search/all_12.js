af20c7ee013cfda935529d6d43f7086ae2e26cda
Dominik Schlösser <dsc@dosc.net>
2022-08-26 14:08:00 +0200

    Update function for sensor

--- docs/search/all_12.js
+++ docs/search/all_12.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['unextend_0',['unextend',['../classustd_1_1Console.html#a10f5616d880010af308a29c08a9c46ad',1,'ustd::Console::unextend(String command)'],['../classustd_1_1Console.html#a0870092447e2ed3f89707446aed5d7ff',1,'ustd::Console::unextend(int commandHandle)']]],
   ['unsubscribe_1',['unsubscribe',['../classustd_1_1Scheduler.html#af22b2534ad447b8da9cb07afc83da261',1,'ustd::Scheduler']]],
-  ['ustd_2',['ustd',['../namespaceustd.html',1,'']]],
-  ['utimeout_3',['utimeout',['../classustd_1_1utimeout.html',1,'ustd::utimeout'],['../classustd_1_1utimeout.html#aae8ff55ed3047600eeba624fbc330179',1,'ustd::utimeout::utimeout()']]]
+  ['update_2',['update',['../classustd_1_1sensorprocessor.html#a6dfdc6b92bb30f947dab9d24dbd4edb7',1,'ustd::sensorprocessor']]],
+  ['ustd_3',['ustd',['../namespaceustd.html',1,'']]],
+  ['utimeout_4',['utimeout',['../classustd_1_1utimeout.html',1,'ustd::utimeout'],['../classustd_1_1utimeout.html#aae8ff55ed3047600eeba624fbc330179',1,'ustd::utimeout::utimeout()']]]
 ];
